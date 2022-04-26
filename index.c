d626593c6ac1a7ce17bd469dc38929c7318e2b0f
teddy1565 <teddy1565@gmail.com>
2022-04-27 01:06:25 +0800

    4ms / 72kb (感覺有更優雅的解法)

---
+++ index.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void) {
+    int n;
+    while(scanf("%d", &n) != EOF) {
+        if (n == 0) {
+            break;
+        }
+        for (int i=1; i<n; i++) {
+            if (i%7 == 0) {
+                continue;
+            }
+            printf("%d ", i);
+        }
+        printf("\n");
+    }
+}
